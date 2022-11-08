e321589814048f5a149891b7d7bacbd1bc38f832
albertofantinato_ <111857261+Albo05@users.noreply.github.com>
2022-11-08 11:10:27 +0100

    Uploaded 8.11.2022 11:09
    v1.0

--- Champions Beer/Champions Beer/Form1.Designer.cs
+++ Champions Beer/Champions Beer/Form1.Designer.cs
@@ -855,7 +855,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(1450, 969);
+            this.ClientSize = new System.Drawing.Size(1163, 686);
             this.Controls.Add(this.panel6);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.panel4);
